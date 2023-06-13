e19dadabb425100604f82bb6f37bcafc1737a641
JoseVi <josgarser>
2023-06-13 19:15:36 +0200

    T2

--- piedra_papel_tijera.py
+++ piedra_papel_tijera.py
@@ -20,8 +20,19 @@ def comprobarGanador(opcionJugador, opcionOrdenador):
     return 0
 
 
+jugador1 = 0
+jugador2 = 0
+
 print("Joc : Pedra, paper y tisora")
-while 1:
+
+while True:
+    intentos = int(input("Dime numero de intentos, tiene que ser impar y mayor de 3 menor de 99: "))
+    if intentos % 2 != 0 and 3 <= intentos <= 99:
+        break
+    else:
+        print("Numero no valido")
+cont = 0
+while cont < intentos:
     compruebaJuega = input("Vols jugar? (s/n): ")
     if 's' in compruebaJuega.lower():
         jugarConOtro = input("Quieres jugar con el ordenador (o) o con otro jugador (j)")
@@ -60,8 +71,10 @@ while 1:
 
                 if comprobarGanador(opcionJugador, elecionOrdenador) == 1:
                     print("Gana l'usuari1 !!!")
+                    jugador1 += 1
                 elif comprobarGanador(opcionJugador, elecionOrdenador) == -1:
                     print("Gana l'ordinador/usuari2 !!!")
+                    jugador2 += 1
                 elif comprobarGanador(opcionJugador, elecionOrdenador) == 0:
                     print("Empate !!!")
                 break
@@ -72,3 +85,12 @@ while 1:
     else:
         print('Entrada incorrecta. Torna a intentar.')
     print()
+    cont+= 1
+
+if "s" in compruebaJuega:
+    if jugador1 > jugador2:
+        print("Ganador total l'usuari1 !!!")
+    elif jugador2 > jugador1:
+        print("Ganador total l'ordinador/usuari2 !!!")
+    else:
+        print("Al final fue Empate !!!")
